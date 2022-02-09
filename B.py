5a3a0b60a6c3deeb483a0120b78408ec8b41c82b
vamsi <vamsikiran1303@gmail.com>
2022-02-09 15:38:51 +0530

    adding files to ABC

---
+++ B.py
@@ -0,0 +1,9 @@
+
+
+for i in range(1,100):
+    if i>1:
+        for j in range(2,i):
+            if i%j ==0:
+                break
+        else:
+            print(i, end=" ")
---
+++ C.py
@@ -0,0 +1,6 @@
+
+a={1:1,2:4,3:9,4:16}
+
+
+squared_dict={x:x**2 for x in range(1,5)}
+print(squared_dict)
